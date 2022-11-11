393805e6e614837131b9031e61424bd2d43eaeff
andreadldev <andrea.delisi2@gmail.com>
2022-11-11 16:47:49 +0100

    Aggiungi immagine

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Hello'
+            message: 'Hello',
+            img: 'img/1.jpg'
         }
     }
 }).mount('#app');
